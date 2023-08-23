55773ce9abe2d686385a4569666fa3e4fd592bc0
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-23 18:41:54 +0300

    minor changes

--- src/components/MainComponents/Main/Main.tsx
+++ src/components/MainComponents/Main/Main.tsx
@@ -1,10 +1,16 @@
+import { Experts } from "../Experts/Experts";
 import { GetStarted } from "../GetStarted/GetStarted";
+import { Healthcare } from "../Healthcare/Healthcare";
+import { StaySafe } from "../StaySafe/StaySafe";
 import "./Main.scss";
 
 export function Main() {
   return (
     <main className="main">
       <GetStarted />
+      <StaySafe />
+      <Experts />
+      <Healthcare />
     </main>
   );
 }
