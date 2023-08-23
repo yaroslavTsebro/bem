b4bb5f25d798dd14b95b98f699f3332a70ee077f
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-23 18:42:33 +0300

    added footer

--- src/components/FooterComponents/Footer/Footer.tsx
+++ src/components/FooterComponents/Footer/Footer.tsx
@@ -1,5 +1,20 @@
 import "./Footer.scss";
+import google from "../../../assets/main/healthcare/apps/google.png";
+import appStore from "../../../assets/main/healthcare/apps/app-store.png";
 
 export function Footer() {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__items">
+          <a className="footer__item">
+            <img src={google} alt="Google play" />
+          </a>
+          <a className="footer__item">
+            <img src={appStore} alt="Apple Store" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
