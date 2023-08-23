5c77f9acd436ecb82f353571492053bc220b58bb
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-23 18:42:25 +0300

    minor changes text block

--- src/components/common/TextBlock/TextBlock.tsx
+++ src/components/common/TextBlock/TextBlock.tsx
@@ -1,5 +1,22 @@
-export function TextBlock() {
-  return (
-    
-  )
+import { ReactNode } from "react";
+import "./TextBlock.scss";
+
+export interface BlockProps {
+  className?: string;
+  title: ReactNode;
+  text: string;
+  children?: ReactNode;
 }
+
+const TextBlock: React.FC<BlockProps> = (props) => {
+  const classes = "text-block " + props.className;
+  return (
+    <div className={classes}>
+      <h2 className="text-block__title">{props.title}</h2>
+      <div className="text-block__text">{props.text}</div>
+      {props.children}
+    </div>
+  );
+};
+
+export default TextBlock;
